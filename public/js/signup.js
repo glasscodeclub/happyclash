7ac4536a779cb9c0ea61242e56a70395b28b4b1f
gcc-sarthak <sarthak.glasscode@gmail.com>
2021-02-19 18:49:24 +0530

    checking for already existing username, checking for equality of passwords, correcting responsiveness of website

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,31 +1,29 @@
 
 
 let k = true;
-let mytimer = setInterval(animate,450);
+let mytimer = setInterval(animate,500);
 
 function animate() {
     let alert = document.querySelector(".alert");
-    if(k == true) {
-        alert.style.backgroundColor = "rgb(240, 188, 188)";
-    } else {
-        alert.style.backgroundColor = "#f8d7da";
-    } 
-
-    (k == true)? k = false: k = true;
+    if(alert)
+    {
+        if(k == true) {
+            alert.style.backgroundColor = "rgb(240, 188, 188)";
+        } else {
+            alert.style.backgroundColor = "#f8d7da";
+        } 
+    
+        (k == true)? k = false: k = true;
+    }
 }
 
 setTimeout(() => {
     clearInterval(mytimer);
-}, 6000);
+}, 4500);
 
-// document.addEventListener("reload",()=> {
-//     let alert = document.querySelector(".alert");
-
-//     alert.style.visibility = "hidden";
-// });
 
 function removeMessage() {
     let alert = document.querySelector(".alert");
 
-    alert.style.visibility = "hidden";
+    alert.remove();
 }
