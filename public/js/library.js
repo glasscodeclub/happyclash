caa0a2472157a442c9657aca041e2085e3cc8caa
gcc-sarthak <sarthak.glasscode@gmail.com>
2021-03-13 21:45:14 +0530

    added video icons to library page

---
+++ public/js/library.js
@@ -0,0 +1,10 @@
+
+let play = document.querySelector('.play');
+let threedots = document.querySelector('.threedots')
+let myvideo = document.querySelector('.myvideo');
+
+play.style.width = myvideo.style.width;
+threedots.style.width = myvideo.style.width;
+
+play.style.height = myvideo.style.height;
+threedots.style.height = myvideo.style.height;
