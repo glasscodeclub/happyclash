3b3bcb09f35bb60578ad50f95a14d68d80310661
Kushagra Keserwani <kushagrak@Kushagras-MacBook-Pro.local>
2021-04-28 13:38:40 +0530

    Video route

---
+++ public/js/Profilemodule/editProfile.js
@@ -0,0 +1,9 @@
+function RemoveClick(params) {
+    console.log(params);
+    console.log("remove button was hit");
+  }
+  
+  function ChangeClick(params) {
+    console.log(params);
+    console.log("Change button was hit");
+  }
