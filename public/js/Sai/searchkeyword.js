1f4a0a8750908fd97d333ea54997b72a572dd814
Sai Ganesh Kodipelly <ganeshrao9949@gmail.com>
2021-05-09 16:55:18 +0530

    add searchkeyword page

---
+++ public/js/Sai/searchkeyword.js
@@ -0,0 +1,15 @@
+function input(e) {
+    console.log(e);
+    console.log("Enter your input");
+    console.log(e.value);
+}
+
+function remove(e) {
+    console.log(e);
+    console.log("U did it!!");
+}
+
+function challenge(e) {
+    console.log(e);
+    console.log("You clicked on challenge button");
+}
