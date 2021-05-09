7221928a1e440953d8a3f15d5d082e0bb473f175
gcc-deepanshu <deepanshu.glasscode@gmail.com>
2021-05-09 22:29:27 +0530

    select all popup + stable + 10may

--- public/js/Clashmodule/clashDetails.js
+++ public/js/Clashmodule/clashDetails.js
@@ -1,5 +1,6 @@
 const openModalButton = document.querySelectorAll('[data-target]')
 const closeModalButton = document.querySelectorAll('[data-close-button]')
+const closeFollowModalButton = document.querySelectorAll('[data-close-button-follow]') 
 const overlay = document.getElementById('overlay')
 
 openModalButton.forEach(button => {
@@ -14,6 +15,13 @@ closeModalButton.forEach(button => {
     button.addEventListener('click', ()=>{
         const modal = button.closest('.popup')
         closeModal(modal)
+    })
+})
+
+closeFollowModalButton.forEach(button => {
+    button.addEventListener('click', ()=>{
+        const modal = button.closest(".popup")
+        closeModalFollow(modal)
     })
 })
 
@@ -33,7 +41,40 @@ function openModal(modal) {
 
 function closeModal(modal) {
     if(modal == null) return
-    console.log(modal)
     modal.classList.remove('active')
     overlay.classList.remove('active')
+}
+
+function closeModalFollow(modal){   
+    if(modal == null) return
+    modal.classList.remove('active')
+}
+
+
+// For Selecting the followers button
+
+// const followers = document.querySelectorAll("[data-select]")
+
+// followers.forEach(follower => {
+//     follower.addEventListener("click", ()=> {
+//         console.log(follower.dataset.select)
+//         const check = document.querySelector(follower.dataset.select)
+//         select(check)
+//     })
+// })
+
+// function select(check){
+//     check.classList.toggle('select')
+// }
+
+function select(e){
+    const checkbox = e.querySelector(".selected")
+    checkbox.classList.toggle('select')
+}
+
+function selectAll(){
+    const select = document.querySelectorAll(".selected")
+    select.forEach( select => {
+        select.classList.toggle('select')
+    } )
 }
