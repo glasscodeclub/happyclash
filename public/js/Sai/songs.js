6fa52c7c170169c146a14d02dc1d944bf29804fe
Sai Ganesh Kodipelly <ganeshrao9949@gmail.com>
2021-05-14 22:23:06 +0530

    done with songs page

---
+++ public/js/Sai/songs.js
@@ -0,0 +1,9 @@
+function challenge(e) {
+    console.log(e);
+    console.log("You clicked on challenge button");
+}
+
+function addButton(e) {
+    console.log(e);
+    console.log("You can do shopping here!");
+}
