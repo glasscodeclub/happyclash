71b334c297f4258acfa5f69c8e9e748dab8aff81
Sai Ganesh Kodipelly <ganeshrao9949@gmail.com>
2021-05-28 20:15:08 +0530

    add event handlers

---
+++ public/js/createclashmodule/whoCanWatch.js
@@ -0,0 +1,19 @@
+function send(e) {
+    console.log(e);
+    console.log("You clicked on send ...");
+}
+
+function seeAll(e) {
+    console.log(e);
+    console.log("You clicked on seeAll...");
+}
+
+function getLink(e) {
+    console.log(e);
+    console.log("You clicked on getlink button...");
+}
+
+function startClash(e) {
+    console.log(e);
+    console.log("This will start the clash...");
+}
